a2a9044bea35d69b65163e862c0e444c6b7847d3
Nicolai Schmid <nicolai@schmid.uno>
2021-04-20 17:01:28 +0200

    feat: allow custom configuration of Errors via constructor

--- src/index.js
+++ src/index.js
@@ -7,70 +7,73 @@ import validateScope from './validateScope';
 
 export { default as validateScope } from './validateScope';
 
-export default class AuthenticationDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field) {
-    const { resolve = defaultFieldResolver } = field;
-    const { scope: requiredScope } = this.args;
+export default function (
+  AuthError = AuthenticationError,
+  ScopeError = AuthorizationError,
+) {
+  return class AuthenticationDirective extends SchemaDirectiveVisitor {
+    visitFieldDefinition(field) {
+      const { resolve = defaultFieldResolver } = field;
+      const { scope: requiredScope } = this.args;
 
-    /* eslint-disable no-param-reassign */
-    field.resolve = async function rslv(...args) {
-      /* eslint-enable no-param-reassign */
-      const [, , { auth }] = args;
+      /* eslint-disable no-param-reassign */
+      field.resolve = async function rslv(...args) {
+        /* eslint-enable no-param-reassign */
+        const [, , { auth }] = args;
 
-      // Kick out any not authenticated user
-      if (!auth) throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out any not authenticated user
+        if (!auth) throw new AuthError('UNAUTHENTICATED');
 
-      // Kick out anyone with empty authentication
-      if (!auth.isAuthenticated)
-        throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out anyone with empty authentication
+        if (!auth.isAuthenticated) throw new AuthError('UNAUTHENTICATED');
 
-      // Kick out anyone with empty authentication
-      if (auth.isAuthenticated === false)
-        throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out anyone with empty authentication
+        if (auth.isAuthenticated === false)
+          throw new AuthError('UNAUTHENTICATED');
 
-      // Setting a required scope is optional
-      if (requiredScope) {
-        if (!validateScope(requiredScope, auth.scope))
-          throw new AuthorizationError('INVALID_SCOPE', {
-            required: requiredScope,
-            provided: auth.scope,
-          });
-      }
+        // Setting a required scope is optional
+        if (requiredScope) {
+          if (!validateScope(requiredScope, auth.scope))
+            throw new ScopeError('INVALID_SCOPE', {
+              required: requiredScope,
+              provided: auth.scope,
+            });
+        }
 
-      return resolve.apply(this, args);
-    };
-  }
+        return resolve.apply(this, args);
+      };
+    }
 
-  visitObject(object) {
-    const { resolve = defaultFieldResolver } = object;
-    const { scope: requiredScope } = this.args;
+    visitObject(object) {
+      const { resolve = defaultFieldResolver } = object;
+      const { scope: requiredScope } = this.args;
 
-    /* eslint-disable no-param-reassign */
-    object.resolve = async function rslv(...args) {
-      /* eslint-enable no-param-reassign */
-      const [, , { auth }] = args;
+      /* eslint-disable no-param-reassign */
+      object.resolve = async function rslv(...args) {
+        /* eslint-enable no-param-reassign */
+        const [, , { auth }] = args;
 
-      // Kick out any not authenticated user
-      if (!auth) throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out any not authenticated user
+        if (!auth) throw new AuthError('UNAUTHENTICATED');
 
-      // Kick out anyone with empty authentication
-      if (!auth.isAuthenticated)
-        throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out anyone with empty authentication
+        if (!auth.isAuthenticated) throw new AuthError('UNAUTHENTICATED');
 
-      // Kick out anyone with empty authentication
-      if (auth.isAuthenticated === false)
-        throw new AuthenticationError('UNAUTHENTICATED');
+        // Kick out anyone with empty authentication
+        if (auth.isAuthenticated === false)
+          throw new AuthError('UNAUTHENTICATED');
 
-      // Setting a required scope is optional
-      if (requiredScope) {
-        if (!validateScope(requiredScope, auth.scope))
-          throw new AuthorizationError('INVALID_SCOPE', {
-            required: requiredScope,
-            provided: auth.scope,
-          });
-      }
+        // Setting a required scope is optional
+        if (requiredScope) {
+          if (!validateScope(requiredScope, auth.scope))
+            throw new ScopeError('INVALID_SCOPE', {
+              required: requiredScope,
+              provided: auth.scope,
+            });
+        }
 
-      return resolve.apply(this, args);
-    };
-  }
+        return resolve.apply(this, args);
+      };
+    }
+  };
 }
